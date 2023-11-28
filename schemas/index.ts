b828b74f6d8922aee94b36423201a561170666cd
Lewis Elborn <lewis@elborn.co>
2023-11-28 22:04:24 +0000

    Update index.ts

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,8 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import tag from './tag'
 import post from './post'
-import service from './service'
-import work from './work'
 import author from './author'
 
-export const schemaTypes = [post, service, work, author, category, tag, blockContent]
+export const schemaTypes = [post, author, category, tag, blockContent]
